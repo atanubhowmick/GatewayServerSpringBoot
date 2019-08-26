f46ac62de71621f211670b33f58b3855d95f9762
Atanu Bhowmick <mail2atanu007@gmail.com>
2019-08-27 00:26:30 +0530

    Eureka client added

--- src/main/java/com/atanu/java/springboot/gateway/GatewaySvcBootApplication.java
+++ src/main/java/com/atanu/java/springboot/gateway/GatewaySvcBootApplication.java
@@ -2,7 +2,15 @@ package com.atanu.java.springboot.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * @author Atanu Bhowmick
+ *
+ */
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class GatewaySvcBootApplication {
 
